3fa772fa4b03a6ccbf2be73e7a46d09d91ec2427
Yurii <shadowjura82@gmail.com>
2023-08-02 10:54:26 +0200

    Initial commit

---
+++ src/main/java/skypro/spring/calculatorJUnit/CalculatorJUnitApplication.java
@@ -0,0 +1,13 @@
+package skypro.spring.calculatorJUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorJUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorJUnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skypro/spring/calculatorJUnit/CalculatorJUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.spring.calculatorJUnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorJUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
