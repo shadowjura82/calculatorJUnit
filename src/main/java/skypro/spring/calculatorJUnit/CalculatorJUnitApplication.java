d19b196f34a815f5d9408a4ae2eadf389c5e1a17
Yurii <shadowjura82@gmail.com>
2023-08-02 10:57:09 +0200

    Initial commit to the new branch

--- src/main/java/skypro/spring/calculatorJUnit/CalculatorJUnitApplication.java
+++ src/main/java/skypro/spring/calculatorJUnit/CalculatorJUnitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculatorJUnitApplication {
 
 	public static void main(String[] args) {
+		// new branch has been created
 		SpringApplication.run(CalculatorJUnitApplication.class, args);
 	}
 
